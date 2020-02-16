ac46fa897a44d1d91fd3f13b49084b8962cf3c1c
andriSH30 <andrisetya93@gmail.com>
2020-02-16 10:25:46 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <p>Mencoba Menggunakan git remote</p>
+    
+</body>
+</html>
